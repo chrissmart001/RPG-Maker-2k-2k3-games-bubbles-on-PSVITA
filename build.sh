c2b872a4560734c670fbb4d170bb5c96712eb936
Humber LaFont'Blank <humberto.fuentealba@gmail.com>
2016-08-31 13:03:00 -0300

    Removed echos statement for untested version

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
- 
 # Abort on error
 set -e
- 
+
 echo -n "Insert vpk title ID [4 characters, uppercase] (Example: AAAA): "
 read unique_id
+
 echo -n "Insert Game Title: "
 read title
  
@@ -13,14 +13,12 @@ unique_id=${unique_id^^}
 # First 4 chars
 unique_id=${unique_id:0:4}
  
-echo "vita-mksfoex -s "TITLE_ID=${unique_id}00001" "$title" build/sce_sys/param.sfo"
-echo "pngquant assets/bg.png -o build/sce_sys/livearea/contents/bg.png"
-echo "pngquant assets/icon0.png -o build/sce_sys/icon0.png"
-echo "pngquant assets/startup.png -o build/sce_sys/livearea/contents/startup.png"
+vita-mksfoex -s "TITLE_ID=${unique_id}00001" "$title" build/sce_sys/param.sfo
 
+pngquant assets/bg.png -o build/sce_sys/livearea/contents/bg.png
+pngquant assets/icon0.png -o build/sce_sys/icon0.png
+pngquant assets/startup.png -o build/sce_sys/livearea/contents/startup.png
 
- 
 echo ${unique_id}00001 > build/titleid.txt
- 
-echo "cd build; zip ../${title}.vpk -r ./eboot.bin ./sce_sys"
 
+cd build; zip ../${title}.vpk -r ./eboot.bin ./sce_sys
